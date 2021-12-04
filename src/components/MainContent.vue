72b1a7bacdfafe9b66f70f535b83ed599f61a17c
Sampob <sampo.bredenberg@metropolia.fi>
2021-12-04 17:09:13 +0200

    Main content consepti. CSS tyylitelty eri kokoisille näytöille.

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="battle-content" class="content-container">
-    <div id="vittusaatana" class="image-container">
-      <img id="showcase_photo" src="../assets/consept3_etusivu.png" alt="winner photo">
-    </div>
     <div class="cup-container">
-      <h3>Test</h3>
+      <div id="cup-image" class="image-container cup-content">
+        <img id="showcase_photo" src="../assets/test.png" alt="winner photo">
+      </div>
+      <div id="cup-string" class="cup-text cup-content">
+        <h3>Cup name</h3>
+        <h4>Cup category</h4>
+        <h4>01:02:22</h4>
+      </div>
     </div>
   </div>
 </template>
@@ -14,10 +18,6 @@ export default {
   name: "MainContent"
 }
 
-window.onload = () => {
-  document.getElementById("vittusaatana").style.height = String(document.getElementById("showcase_photo").offsetHeight + "px");
-}
-
 </script>
 
 <style scoped>
@@ -25,18 +25,41 @@ window.onload = () => {
 .content-container {
   height: 100%;
   padding: 0.5rem 0.5rem 64px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: auto;
+  margin-bottom: auto;
 }
 
-.image-container {
-  margin: 2% auto 1% auto;
-  border: 1px solid #111111;
-  width: 60%;
+.cup-content {
+  display: flex;
+  max-width: 100%;
   height: auto;
 }
 
 .image-container img{
-  width: 100%;
+  max-width: 100%;
   height: auto;
+  border: 1px solid #111111;
+  max-height: 640px;
+  display: block;
+  margin-bottom: 10px;
+}
+
+.cup-container {
+  max-width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 20px auto 10px auto;
+}
+
+.cup-text {
+  width: auto;
+  background-color: #404040;
+  justify-content: space-around;
+  color: white;
 }
 
 </style>
