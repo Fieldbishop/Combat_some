5e1dd0fbd6c8bba62d3fc777d635f9edfd96a981
Fieldbishop <74423885+Fieldbishop@users.noreply.github.com>
2021-12-08 05:10:37 +0200

    new user postaaja

--- server/server-new.js
+++ server/server-new.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 //const bodyParser = require('body-parser');
@@ -15,10 +14,10 @@ const corsOptions = require('./server_modules/cors-local');
 app.use(express.json());
 
 // for parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 // for parsing multipart/form-data
-app.use(multer({storage : storage}).single('image'));
+app.use(multer({storage: storage}).single('image'));
 //app.use(express.static('public'));
 
 
@@ -36,7 +35,7 @@ console.log(http.STATUS_CODES);
 */
 
 app.options('*', cors(corsOptions));
-app.post('/api/postTrigger', cors(corsOptions),function (req, res){
+app.post('/api/postTrigger', cors(corsOptions), function (req, res) {
     console.log(req.headers);
     console.log(req.url);
     console.log(req.ip);
@@ -50,34 +49,52 @@ app.post('/api/postTrigger', cors(corsOptions),function (req, res){
     console.log(req.subdomains);
     res.status(204).end()
 })
-app.post('/api/image', cors(corsOptions), function(req,res){
+app.post('/api/image', cors(corsOptions), function (req, res) {
     //console.log(req);
-    (async () =>{
-        if(req.body){
+    (async () => {
+        if (req.body) {
             req.body
             res.status(200).end()
-        } else{
+        } else {
             res.status(403).end()
         }
     })()
 })
 
 // L juttuja
-app.get('/api/leaderboard',function(req,res){
+/** Returns a leaderboard dataset from the database */
+app.get('/api/leaderboard', function (req, res) {
     (async () => {
-        if(req.body){
+        if (req.body) {
             let query = "SELECT userName FROM user ORDER BY wins DESC";
             let leaderboard = await mysql.mysqlQuery(query, null, "get");
             res.send(leaderboard);
             res.status(200).end();
-        } else{
+        } else {
             res.status(403).end();
         }
     })();
 });
 
-app.get('/api/test', (req,res) =>{
-    res.send("testitesti")
+/** Inserts a new user into database on registration */
+app.post('/api/createUser', cors(corsOptions), function (req, res) {
+    //console.log(req);
+    (async () => {
+        if (req.body) {
+            let query = "INSERT INTO user VALUES('?','?','0','0')";
+            let args = "";                                       // mistäs nää nyt otettiin :)
+            await mysql.mysqlQuery(query, args, "post");
+            res.status(200).end();
+        } else {
+            res.status(403).end();
+        }
+    })();
+});
+
+app.get('/api/test', (req, res) => {
+    let query = "INSERT INTO user VALUES('?','?','0','0')";
+    console.log(query);
+    res.send(query)
 })
 
 /**
@@ -88,12 +105,10 @@ app.get('/api/test', (req,res) =>{
  */
 
 
-
-
-app.get('/events', function (req, res){
+app.get('/events', function (req, res) {
     res.sendFile(path.join(__dirname + '/listofevents.html'));
 })
-app.get('/api/events/all',corsOptions, function (req, res){
+app.get('/api/events/all', corsOptions, function (req, res) {
     let sql = "SELECT event_date.Date, event.Name, event.Type,"
         + " Location.Location_name"
         + " FROM event_date, event, location"
@@ -101,7 +116,7 @@ app.get('/api/events/all',corsOptions, function (req, res){
         + " event.Location_Location_id = Location.Location_id"
         //+ " and event_date.Date >= ? and event_date.Date <= ?"
         + " GROUP BY Name"
-                + " ORDER BY event_date.Date";
+        + " ORDER BY event_date.Date";
     (async () => { // IIFE (Immediately Invoked Function Expression)
         const rows = mysql.query(sql);
         console.log(rows);
@@ -109,19 +124,19 @@ app.get('/api/events/all',corsOptions, function (req, res){
     })()
 })
 
-app.get('/api/location', corsOptions, function (req, res){
+app.get('/api/location', corsOptions, function (req, res) {
     let parsedUrlQuery = url.parse(req.url, true).query;
     console.log(parsedUrlQuery);
     let locationName = parsedUrlQuery.name;
     let sql = "SELECT * " +
         "FROM location WHERE Location.Location_name = ?";
-    (async ()=>{ // IIFE (Immediately Invoked Function Expression)
-        let rows = await mysql.mysqlQuery(sql,[locationName])
+    (async () => { // IIFE (Immediately Invoked Function Expression)
+        let rows = await mysql.mysqlQuery(sql, [locationName])
         console.log(rows);
-        if(rows !== undefined){
+        if (rows !== undefined) {
             res.send('{"numOfRows":' + rows.length + ',"rows":' + JSON.stringify(rows) + '}');
-        } else{
-            res.send('{"rows": '+rows+'}');
+        } else {
+            res.send('{"rows": ' + rows + '}');
         }
     })()
 })
@@ -136,43 +151,44 @@ app.post('/api/event', corsOptions, function (req, res) {
             event_date: false,
         };
         let locationId = await locationSearch(req);
-        if(!isNaN(locationId)) message.location = true;
+        if (!isNaN(locationId)) message.location = true;
         let sqlForAddingEvent = "INSERT " +
             "INTO event VALUES(?, ?, ?, ?)";
         let event
-        if(req.body.eventName && req.body.type && !isNaN(locationId)){
+        if (req.body.eventName && req.body.type && !isNaN(locationId)) {
             event = await mysql.mysqlQuery(sqlForAddingEvent, [null, req.body.eventName, req.body.type, locationId]);
-            if(!isNaN(event.insertId)) message.event = true;
+            if (!isNaN(event.insertId)) message.event = true;
             console.log(event);
-            let event_date = await mysql.mysqlQuery("INSERT INTO event_date VALUES(?, ?)", [req.body.date,event.insertId]);
-            if(!isNaN(event_date.insertId)) message.event_date = true;
+            let event_date = await mysql.mysqlQuery("INSERT INTO event_date VALUES(?, ?)", [req.body.date, event.insertId]);
+            if (!isNaN(event_date.insertId)) message.event_date = true;
             console.log(event_date);
         }
         res.send(JSON.stringify(message));
     })()
 })
-async function locationSearch(req){
+
+async function locationSearch(req) {
     let location = await mysql.mysqlQuery("SELECT * " +
         "FROM location WHERE Location_name = ? & Street_address = ? & Country = ? ORDER BY Location_id DESC LIMIT 0, 1",
         [req.body.placeName, req.body.address, req.body.country])
     let locationObject;
     locationObject = IsJsonString(location);
     let locationId = null;
-    if("Location_id" in locationObject){
+    if ("Location_id" in locationObject) {
         locationId = locationObject.Location_id;
-    } else if(!req.body.placeName || !req.body.city || !req.body.zip ||
-        !req.body.country || !req.body.address){
+    } else if (!req.body.placeName || !req.body.city || !req.body.zip ||
+        !req.body.country || !req.body.address) {
         location = await mysql.mysqlQuery("SELECT * " +
             "FROM location ORDER BY Location_id DESC LIMIT 0, 1");
         locationObject = IsJsonString(location);
-        if("Location_id" in locationObject){
+        if ("Location_id" in locationObject) {
             locationId = locationObject.Location_id;
         }
     } else {
         let sqlForAddingLocation = "INSERT " +
             "INTO location VALUES(?, ?, ?, ?, ?, ?)";
         let response = await mysql.mysqlQuery(sqlForAddingLocation,
-            [null ,req.body.placeName, req.body.address, req.body.city, req.body.zip, req.body.country]);
+            [null, req.body.placeName, req.body.address, req.body.city, req.body.zip, req.body.country]);
         locationId = response.insertId;
     }
     return locationId;
@@ -180,19 +196,20 @@ async function locationSearch(req){
 
 function IsJsonString(jsonString) {
     try {
-        if(jsonString && typeof jsonString === "object"){
+        if (jsonString && typeof jsonString === "object") {
             return jsonString;
         }
         let jsonObject = JSON.parse(jsonString);
         if (jsonObject && typeof jsonObject === "object") {
             return jsonObject;
         }
+    } catch (e) {
+        console.log("Caught an error: " + e + "while parsing " + jsonString)
     }
-    catch (e) { console.log("Caught an error: " + e + "while parsing "+jsonString) }
     return false;
 }
 
-app.get('/api/events', corsOptions, function (req, res){
+app.get('/api/events', corsOptions, function (req, res) {
 
     let parsedUrlQuery = url.parse(req.url, true).query;
     let startDate = parsedUrlQuery.start;
@@ -205,7 +222,7 @@ app.get('/api/events', corsOptions, function (req, res){
         + " and event_date.Date >= ? and event_date.Date <= ?"
         + " GROUP BY Name"
         + " ORDER BY event_date.Date";
-    (async ()=> {
+    (async () => {
         let rows = await mysql.mysqlQuery(sql, [startDate, endDate]);
         console.log(rows);
         res.send('{"numOfRows":' + rows.length + ',"rows":' + JSON.stringify(rows) + '}');
--- server/server_modules/mysql-connection.js
+++ server/server_modules/mysql-connection.js
@@ -12,27 +12,28 @@ let con = mysql.createConnection({
 // node native promisify
 const query = util.promisify(con.query).bind(con); // is bind needed?
 
-con.connect(function(err) {
-    if(err) {
+con.connect(function (err) {
+    if (err) {
         console.log("failed to connect with error message :");
         throw err;
     }
     console.log("Connected to MySQL");
 })
+
 async function mysqlQuery(sql, arg, httpVerb) {
     try {
-        if(arg === undefined || arg === null){
+        if (arg === undefined || arg === null) {
             return await query(sql);
         }
         return await query(sql, [...arg]);
-    } catch (err){
-        if(httpVerb){
-            if(httpVerb.toLowerCase() === "post"){
+    } catch (err) {
+        if (httpVerb) {
+            if (httpVerb.toLowerCase() === "post") {
                 console.log("Post wasn't successful..")
-            } else if(httpVerb.toLowerCase() === "get"){
+            } else if (httpVerb.toLowerCase() === "get") {
                 console.log("Failed to get..")
             }
-        } else{
+        } else {
             console.log("Database error!" + err);
         }
     }
