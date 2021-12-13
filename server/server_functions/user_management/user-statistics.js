97c047523e958a8c3cd493c87ad807af525bb06c
Sampob <sampo.bredenberg@metropolia.fi>
2021-12-13 17:13:57 +0200

    Käyttäjän osallistumisten päivitys tietokantaan

--- server/server_functions/user_management/user-statistics.js
+++ server/server_functions/user_management/user-statistics.js
@@ -16,4 +16,19 @@ function userWins(req, res) {
   })()
 }
 
+function updateUserSubmissions(req, res) {
+  const username = jwt.verify(req.body.token, "secret").username;
+
+  const query = "UPDATE user set participations = participations+1 WHERE userName = ?";
+
+  (async () => {
+    const mysqlResponse = await mysql.mysqlQuery(query, username, 'patch');
+    const status = mysqlHelpers.httpStatusWithSqlResponse(mysqlResponse);
+    return res.status(status).send({
+      "username": username
+    })
+  })();
+}
+
 module.exports.userWins = userWins;
+module.exports.updateUserSubmissions = updateUserSubmissions;
