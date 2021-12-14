ca2764bb21b6d4140606e6646051eafbcd5f45c5
Sampob <sampo.bredenberg@metropolia.fi>
2021-12-14 18:25:14 +0200

    Responsiivisuutta ja näkyvyyttä elementteihin

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,29 +4,42 @@
       <div class="nav-container">
         <div class="navbar-left">
           <a href="/" style="float: left;">
-            <span>
-              <img id="logo" src="../assets/fist_logo.png" alt="imagebattle">
+            <span class="material-icons" style="font-size: 50px">
+              thumb_up
+              <!-- <img id="logo" src="../assets/fist_logo.png" alt="imagebattle"> -->
             </span>
           </a>
           <a class="nav-link" id="leaderboards" @click="$emit('modal', 0)"><b>Leaderboards</b></a>
         </div>
         <div id="navbar-user" class="navbar-right" v-if="loggedIn === false">
           <ul>
-            <li>
-              <a class="nav-link" @click="$emit('modal', 1)"><b>Log in</b></a>
+            <li class="log-text" @click="$emit('modal', 1)">
+              <a class="nav-link"><b>Log in</b></a>
             </li>
-            <li>
-                <a class="nav-link" @click="$emit('modal', 2)"><b>Sign up</b></a>
+            <li class="sign-text" @click="$emit('modal', 2)">
+                <a class="nav-link"><b>Sign up</b></a>
+            </li>
+            <li class="log-icon" @click="$emit('modal', 1)">
+              <span class="material-icons">login</span>
+            </li>
+            <li class="sign-icon" @click="$emit('modal', 2)">
+              <span class="material-icons">person_add</span>
             </li>
           </ul>
         </div>
         <div id="navbar-logged" class="navbar-right" v-else>
           <ul>
-            <li>
-              <a class="nav-link" @click="$emit('modal', 3)"><b>{{username}}</b></a>
+            <li class="log-text" @click="$emit('modal', 3)">
+              <a class="nav-link"><b>{{username}}</b></a>
             </li>
-            <li>
-              <a class="nav-link" @click="signOut()"><b>Sign out</b></a> <!-- this.loggedIn = false -->
+            <li class="sign-text" @click="signOut()">
+              <a class="nav-link"><b>Sign out</b></a> <!-- this.loggedIn = false -->
+            </li>
+            <li class="log-icon" @click="$emit('modal', 3)">
+              <span class="material-icons">account_circle</span>
+            </li>
+            <li class="sign-icon" @click="signOut()">
+              <span class="material-icons">logout</span>
             </li>
           </ul>
         </div>
@@ -83,18 +96,17 @@ export default {
   justify-content: space-evenly;
 }
 
-#logo{
-  max-width: 50px;
-  max-height: 50px;
-}
-
 .navbar-left {
   align-items: center;
   display: flex;
 }
 
+.navbar-left .nav-link{
+  margin-left: 0;
+}
+
 .navbar-left * {
-  margin: 10px;
+  margin: 5px;
 }
 
 @media (max-width: 590px) {
@@ -102,8 +114,26 @@ export default {
     justify-content: space-around;
   }
 
+  .navbar-left .nav-link{
+    padding: 0;
+  }
+
+  .log-text, .sign-text {
+    display: none;
+  }
+
+  .log-icon, .sign-icon {
+    display: flex;
+  }
+
   .navbar-right .nav-link {
     padding: 10px;
+  }
+}
+
+@media (min-width: 591px) {
+  .log-icon, .sign-icon {
+    display: none;
   }
 }
 
@@ -117,8 +147,45 @@ export default {
   overflow: hidden;
 }
 
-.navbar-right ul li {
-  display: inline-block;
+.log-text, .sign-text {
+  background-color: #d33c40;
+  margin-right: 10px;
+  padding: 8px;
+  border-radius: 25px;
+  text-overflow: ellipsis;
+  max-width: 100px;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.log-icon, .sign-icon {
+  margin-right: 15px;
+  background-color: #d33c40;
+  padding: 10px;
+  border-radius: 25px;
+  align-items: center;
+  justify-content: center;
+}
+
+.log-icon:hover .material-icons, .sign-icon:hover .material-icons {
+  color: white;
+}
+
+.log-icon:hover, .sign-icon:hover {
+  cursor: pointer;
+}
+
+.log-icon .material-icons, .sign-icon .material-icons {
+  color: black;
+  font-size: 33px;
+}
+
+.log-icon:hover {
+  background-color: #888888;
+}
+
+.sign-icon:hover {
+  background-color: #888888;
 }
 
 .navbar-right ul form input {
@@ -136,6 +203,10 @@ export default {
 .nav-link:hover {
   color: cadetblue;
   cursor: pointer;
+}
+
+a .material-icons:hover {
+  color: white;
 }
 
 </style>
